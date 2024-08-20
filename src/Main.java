4c3bde11ac8eac9817c0dd2640f80a1bd1fc094a
Lefchakk <joao.lefchak@edu.unipar.br>
2024-08-19 22:44:53 -0300

    Atividade_01

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+    public static void main(String[] args) {
+       ////Declaração de variáveis////
+        int idade= 30;
+        int Idade= 20;
+        double altura= 1.75;
+        String Nome= "João Gabriel";
+
+        boolean estuding = true;
+        String estud= "Estudante";
+
+        if (estuding) {
+
+        }
+        ////Operações simples/////
+
+        int soma=idade+Idade;
+        double multiplicacao=altura*2;
+
+        ////Declaração de resultados////
+        System.out.println("Eu sou: " + Nome);
+        System.out.println("Sou um: " + estud);
+        System.out.println("Minha idade é: " + Idade);
+        System.out.println("Minha altura é: " + altura);
+        System.out.println("A soma das idades é:" + soma);
+        System.out.println("A multiplicação de altura é; " + multiplicacao);
+
+
+    }
+}
